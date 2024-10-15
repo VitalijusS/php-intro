305bad7d7655acd0c8cf055d0d582ae0055538ca
VitalijusS <v.stifanovicius@gmail.com>
2024-10-15 15:11:15 +0300

    intro

--- php.php
+++ php.php
@@ -1,2 +1,18 @@
-<?php
-echo 'Hello world!';
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Header</h1>
+    <h2>
+        <?php echo 'H2 PHP'; ?>
+    </h2>
+    <h3>A dice roll: <?php echo rand(1, 6); ?></h3>
+</body>
+
+</html>
